ea6d55e718daab6523a16ea3d130d88b9c94543b
lena174 <khrkhryanl@gmail.com>
2024-11-14 21:08:45 +0400

    Home work-orange website

---
+++ cypress/e2e/Orange2Home-Work/OrangeHomeWork2.cy.js
@@ -0,0 +1,8 @@
+
+// Adding a New User in the Admin Section
+// Login with valid admin credentials.
+// Open the Admin section from the left sidebar.
+// Navigate to the "User Management" tab and click "Add User."
+// Fill in required fields (e.g., Employee Name, Username, Role, Password).
+// Click the "Save" button.
+// Verify the new user appears in the user list.
